eac29b95d67d5aa89f7bc37349430be420e93009
doanduyvan <duyvanlee2001@gmail.com>
2024-11-01 09:30:08 +0700

    clear

--- public/js/Users/mainUsers.js
+++
@@ -1,8 +0,0 @@
-
-import { mbConfirm } from '../allmodule.js';
-const idlogout = document.getElementById('mblogout');
-idlogout.onclick = async (e) => {
-    e.preventDefault();
-    const check = await mbConfirm('Are you sure you want to log out?');
-    if(check) window.location.href = idlogout.href;
-}
